1d4d57b5fa953bcfe83e4abdcae524f32edc29fd
Fausto24108 <fpoggi24@hotmail.com>
2025-10-08 16:52:20 -0300

    Termino Modo Noche.

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,31 @@
 let ModoDN = false;
 let Dia = "rgb(233, 223, 223)";
 let Noche = "black";
+let ContenedorDia = "whitesmoke";
+let ContenedorNoche = "rgb(60, 60, 60)";
+let FooterDia = "rgba(102, 55, 211, 0.966)";
+let FooterNoche = "grey";
 
 function Modo(){
     const Barra = document.getElementsByClassName("Barra_navegacion")[0]; 
-    
+    const Contenedor = document.getElementsByClassName("Contenedor")[0]; 
+    const Parrafos = document.getElementsByClassName("Descripcion"); 
+    const Footer = document.getElementsByTagName("footer")[0]; 
+
+
     if(ModoDN){
         document.body.style.backgroundColor = Dia;
         Barra.style.backgroundColor = "white";
         Barra.style.borderTop = "3px solid crimson";
         Barra.style.borderBottom = "3px solid crimson";
-        ModoDN = "false";
+        Contenedor.style.backgroundColor = ContenedorDia;
+        Footer.style.backgroundColor = FooterDia;
+
+        for(let i = 0; i<Parrafos.length; i++){
+        Parrafos[i].style.color = "black";
+        }
+
+        ModoDN = false;
     }
 
     else{
@@ -18,8 +33,14 @@ function Modo(){
         Barra.style.backgroundColor = "black";
         Barra.style.borderTop = "3px solid black";
         Barra.style.borderBottom = "3px solid black";
+        Contenedor.style.backgroundColor = ContenedorNoche;
+        Footer.style.backgroundColor = FooterNoche;
+
+        for(let i = 0; i<Parrafos.length; i++){
+        Parrafos[i].style.color = "white";
+        }
+
         ModoDN = true;
     }
     
-    Barra.style.backgroundColor = color;
 }
