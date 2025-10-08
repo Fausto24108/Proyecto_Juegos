5552e1f583342c970c409ba6ad1020fb4947ea4c
Fausto24108 <fpoggi24@hotmail.com>
2025-10-08 16:12:17 -0300

    JS fondo.

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,24 @@
-let ColorActual = "";
+let ModoDN = false;
+let Dia = "rgb(233, 223, 223)";
+let Noche = "black";
 
 function Modo(){
     const Barra = document.getElementsByClassName("Barra_navegacion")[0]; 
     
-    if(ColorActual === "black"){
+    if(ModoDN){
+        document.body.style.backgroundColor = Dia;
         Barra.style.backgroundColor = "white";
         Barra.style.borderTop = "3px solid crimson";
         Barra.style.borderBottom = "3px solid crimson";
-        ColorActual = "";
+        ModoDN = "false";
     }
 
     else{
+        document.body.style.backgroundColor = Noche;
         Barra.style.backgroundColor = "black";
         Barra.style.borderTop = "3px solid black";
         Barra.style.borderBottom = "3px solid black";
-        ColorActual = "black";
+        ModoDN = true;
     }
     
     Barra.style.backgroundColor = color;
